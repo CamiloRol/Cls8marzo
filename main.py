618c0828397a4b022d65f0ba9d5b7aaedaa53c12
DESARROLLO <desarrollo@montolivo.com>
2025-03-08 10:54:29 -0500

    another changes on main

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 from cls.register import Register
+
+id = int(input("Ingrese su numero de documento de identidad: "))
+name = input("Ingrese su primer nombre: ")
+last_name = input("Ingrese su primer apellido: ")
+email = input("Ingrese su correo electronico: ")
+password = input("Ingrese un contraseña minimo 8 caracteres entre numeros y letras con un caracter especial: ")
+
+passwordEncoded = password.encode('utf-8')
+
+formRegister = Register(id, name, last_name, email, passwordEncoded)
+formRegister.user_register()
